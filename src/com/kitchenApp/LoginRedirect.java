a59b2bafa4d0f8b565beb5eaba10ed4932931fde
afaherty27 <afaherty27@gmail.com>
2015-10-15 02:20:48 -0500

    first attempt at redirecting to secured dashboard

--- src/com/kitchenApp/LoginRedirect.java
+++ src/com/kitchenApp/LoginRedirect.java
@@ -1,8 +1,11 @@
 package com.kitchenApp;
 
+import com.kitchenApp.database.*;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.io.IOException;
 
 /**
  * @author afaherty
@@ -15,29 +18,66 @@ import javax.servlet.annotation.*;
 
 public class LoginRedirect extends HttpServlet {
 
+    private DatabaseConnection databaseConnection;
+    private Search search;
+    private Employee employee = new Employee();
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        ServletContext servletContext = getServletContext();
+        search = new Search();
+
+        // get database connection from servlet context
+        databaseConnection = (DatabaseConnection) servletContext.getAttribute(
+                "dataBaseConnection");
+
+        placeLogInSession(request, search);
+
+        forwardToDashboard(request, response);
+    }
+
+    /**
+     *  Places Search object into session
+     * @param request HttpServletRequest objuect
+     * @param search Search object
+     */
+    private void placeLogInSession(HttpServletRequest request, Search search) {
+
+        HttpSession loginSession = request.getSession();
+        loginSession.setAttribute("loginSession", search);
+    }
+
+    public void forwardToDashboard(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
         String url = "";
+       // RequestDispatcher dispatcher;
 
+        if (employee.getUserRole() == "chef") {
+            url = "/chef/chef.jsp";
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+            dispatcher.forward(request, response);
+        }
         /*
         PSUEDO CODE
         if SQL user_role value is chef {
+            url = "/chef/chef.jsp";
             redirect to chef/chef.jsp
         }
 
         if SQL user_role value is line_cook {
+            url = "/lineCook/lineCook.jsp";
            redirect to lineCook/lineCook.jsp
         }
 
         if SQL user_role value is general_manager {
+            url = "gm/generalManager.jsp";
             redirect to gm/generalManager.jsp
         }
 
         if SQL user_role value is dishwasher {
+            url = "/dish/dish.jsp";
             redirect to dish/dish.jsp
         }
          */
-
     }
 }
