4863bb09c529cfa14bb4ab623b59d7d7de9eaff4
afaherty27 <afaherty27@gmail.com>
2015-10-31 22:35:20 -0500

    initial commit

---
+++ src/com/kitchenApp/com/kitchenApp/database/DBConnection.java
@@ -0,0 +1,25 @@
+package com.kitchenApp.com.kitchenApp.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * Creates a connection to the mySQL database
+ * @author afaherty
+ */
+
+public class DBConnection {
+
+    private Connection establishConection() throws SQLException, ClassNotFoundException {
+
+        String url = "jdbc:mysql://localhost/student";
+        String userName = "student";
+        String userPassword = "student";
+
+        Connection connection = DriverManager.getConnection(url, userName, userPassword);
+
+        return connection;
+    }
+}
