486319dc4881ca44603ca6d6332c334244bba83d
afaherty27 <afaherty27@gmail.com>
2015-11-02 15:03:02 -0600

    completed provider

---
+++ src/com/kitchenApp/database/SessionFactoryProvider.java
@@ -0,0 +1,49 @@
+package com.kitchenApp.database;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+
+/**
+ * @author afaherty
+ */
+
+public class SessionFactoryProvider {
+
+    private static SessionFactory sessionFactory;
+
+    /**
+     * Instantiates a new SessionFactory object.  Loads configuration details.
+     */
+    public static void  createSessionFactory() {
+
+        Configuration config = new Configuration(); // new Configuration obj
+
+        // use mappings and properties specified in hibernate.cfg.xml
+        config.configure();
+
+        //build on ServiceRegistry inst. Apply value of config var
+        ServiceRegistry registry = new ServiceRegistryBuilder().applySettings(
+                     //apply all properties
+                config.getProperties()).buildServiceRegistry();
+
+        //instantiate new SessionFactory using properties & mappings
+        sessionFactory = config.buildSessionFactory(registry);
+    }
+
+    /**
+     * Instantiates a SessionFactory if none exists
+     * @return sessionFactory
+     */
+    public static SessionFactory getSessionFactory() {
+
+        if(sessionFactory == null) {
+
+            createSessionFactory();
+        }
+
+        return sessionFactory;
+    }
+}
