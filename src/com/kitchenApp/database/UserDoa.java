3039f7ce729c1858c66a38a2a8296f12677ba32a
afaherty27 <afaherty27@gmail.com>
2015-11-02 20:21:28 -0600

    removed interface implementation. changed class name

--- src/com/kitchenApp/database/UserDoa.java
+++ src/com/kitchenApp/database/UserDoa.java
@@ -1,20 +1,53 @@
 package com.kitchenApp.database;
 
 
-import java.util.List;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.HibernateException;
+
 
 /**
- * DOA interface for User objects
  *
  * @author afaherty
- * 10/30/2015
  */
 
-public interface UserDoa {
+public class UserDoa {
 
-    List<User> getAllUsers();
-    User getUser(int userId);
-    void addUser(User user);
-    void updateUser(User user);
-    void deleteUser(User user);
+    private final Logger logger = Logger.getLogger(UserDoa.class);
+
+    public User addUser(String userName, String userPass, String address, int phone,
+                        String email, int social) {
+
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction transaction = null;
+        Integer userId = 0;
+
+        try {
+            transaction = session.beginTransaction();
+            User user = new User(userName, userPass, address, email, phone, social);
+            userId = (int) session.save(user);
+
+            transaction.commit();
+        } catch (HibernateException e){
+            if (transaction != null) {
+                transaction.rollback();
+                //logger.info(e.printStackTrace());
+            }
+        } finally {
+            session.close();
+        }
+
+        return null; // TODO change value
+    }
+
+    // TODO getUser(int userId)
+
+    // TODO getAllUsers()
+
+    // TODO updateUser(User user)
+
+    // TODO deleteUser(User user)
+
+
 }
