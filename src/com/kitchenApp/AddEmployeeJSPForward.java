32795be752d240e641d58d493ed5f4ee82c8d254
afaherty27 <afaherty27@gmail.com>
2015-11-05 22:36:35 -0600

    completed addEmployee jsp forwarding servlet

---
+++ src/com/kitchenApp/AddEmployeeJSPForward.java
@@ -0,0 +1,44 @@
+package com.kitchenApp;
+
+import org.apache.log4j.Logger;
+import org.omg.CORBA.Request;
+
+import javax.servlet.annotation.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.io.IOException;
+
+/**
+ * @author afaherty
+ * Date: 11/5/2015
+ */
+@WebServlet(
+        name = "addEmployeeForm",
+        urlPatterns = {"/chef/addEmployee"}
+)
+
+/**
+ * forwarding class for addEmployee.jsp page
+ */
+public class AddEmployeeJSPForward extends HttpServlet {
+
+    private final Logger log = Logger.getLogger(AddEmployeeJSPForward.class);
+
+    /**
+     *
+     * @param request HttpServletRequest object
+     * @param response HttpServletResponse object
+     * @throws ServletException if there is a servlet error
+     * @throws IOException if there is an input output error
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String url = "/chef/addEmployee.jsp";
+
+        log.info("Accessing: " + url);
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(request, response);
+    }
+}
