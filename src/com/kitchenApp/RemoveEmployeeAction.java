856770ce772892116bd82e5b04937c43c9c4a1fe
afaherty27 <afaherty27@gmail.com>
2015-11-23 03:25:07 -0600

    added redirect method. javadoc. log4j set up.

--- src/com/kitchenApp/RemoveEmployeeAction.java
+++ src/com/kitchenApp/RemoveEmployeeAction.java
@@ -1,20 +1,57 @@
 package com.kitchenApp;
 
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author afaherty
  * @version 1.0 on 11/23/2015
  */
 
-public class RemoveEmployeeAction {
+@WebServlet(
+        name = "removeEmployeeServlet",
+        urlPatterns = "/chef/removeUser"
+)
 
-    //TODO recieve value entered in text box. parse to Int.
+public class RemoveEmployeeAction extends HttpServlet {
 
-    // TODO display list of employees on button click
+    private final Logger log = Logger.getLogger(RemoveEmployeeAction.class)
+    /**
+     * Handles HTTP post request
+     * @param request the HttpServletRequest object
+     * @param response the HttpServletResponse object
+     * @throws ServletException if there is a Servlet failure
+     * @throws IOException if there is an IO failure
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-    // TODO remove employee from database. Confirm before delete?????
+        //TODO recieve value entered in text box. parse to Int.
 
-    // TODO display successful deletion message.
+        // TODO display list of employees on button click
 
-    // TODO return user to refreshed page on submit
+        // TODO remove employee from database. Confirm before delete?????
 
+        // TODO display successful deletion message.
+
+        redirectOnSubmit(response);
+
+    }
+    /**
+     * Redirects user to clean form after submit. posts message for successful add
+     * @param response HttpServletResponse object
+     * @throws ServletException if there is a servlet error
+     * @throws IOException if there is an input/output error
+     */
+    public void redirectOnSubmit(HttpServletResponse response) throws ServletException, IOException {
+
+        String url = "/chef/removeEmployee";
+        response.sendRedirect(url);
+    }
 }
