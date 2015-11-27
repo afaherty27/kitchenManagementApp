0d7a6d9d7bc98a6d46c791bab0de91e86136ae3f
afaherty27 <afaherty27@gmail.com>
2015-11-26 19:28:44 -0600

    added getUserRole method

--- src/com/kitchenApp/database/dataAccess/UserRoleDao.java
+++ src/com/kitchenApp/database/dataAccess/UserRoleDao.java
@@ -1,9 +1,13 @@
 package com.kitchenApp.database.dataAccess;
 
 import com.kitchenApp.database.entity.UserRole;
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Transaction;
 import org.hibernate.Session;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author afaherty
@@ -39,5 +43,76 @@ public class UserRoleDao {
         }
 
         return userRoleId;
+    }
+
+    /**
+     * To retrieve user role data relating to an individual employee
+     * @param userRoleId reference to user role id in database
+     * @return userRoleId
+     */
+    public UserRole getUserRole(int userRoleId) {
+
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        UserRole userRole = null;
+
+        try {
+            userRole = (UserRole) session.get(UserRole.class, userRoleId);
+
+            if (userRole != null) {
+                return userRole;
+            }
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return userRole;
+    }
+
+    // TODO getAllUsers()
+    public List getUserRoleList() {
+
+        List<UserRole> userRoles = new ArrayList<>();
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+
+        //criteria
+        Criteria criteria = session.createCriteria(UserRole.class);
+        try {
+
+            userRoles = criteria.list();
+        } catch (HibernateException e) {
+
+            e.printStackTrace();
+        } finally {
+
+            session.close();
+        }
+
+        return userRoles;
+    }
+
+    // TODO updateUser(User user)
+    public void updateUserRole(UserRole userRole) {
+
+    }
+
+    public void deleteUserRole(Integer userRoleId) {
+
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction trans = null;
+
+        try {
+            trans = session.beginTransaction();
+            UserRole userRole = (UserRole)session.get(UserRole.class, userRoleId);
+            session.delete(userRole);
+            trans.commit();
+        } catch (HibernateException e) {
+            if (trans != null) {
+                trans.rollback();
+            }
+        } finally {
+            session.close();
+        }
+
     }
 }
