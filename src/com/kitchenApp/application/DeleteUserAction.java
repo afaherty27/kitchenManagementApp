fefbb98354d152beed8414e96ca1272ba264a64c
afaherty27 <afaherty27@gmail.com>
2015-11-27 02:18:11 -0600

    reorganized prologue

--- src/com/kitchenApp/application/DeleteUserAction.java
+++ src/com/kitchenApp/application/DeleteUserAction.java
@@ -1,9 +1,5 @@
 package com.kitchenApp.application;
 
-import com.kitchenApp.database.dataAccess.UserDao;
-import com.kitchenApp.database.dataAccess.UserRoleDao;
-import com.kitchenApp.database.entity.User;
-import com.kitchenApp.database.entity.UserRole;
 import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
@@ -13,17 +9,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(
+        name = "deleteUser",
+        urlPatterns = "/chef/deleteUser"
+)
+
 /**
  * @author afaherty
  * @version 1.0 on 11/23/2015
  */
-
-@WebServlet(
-        name = "employeeAction",
-        urlPatterns = "/chef/employeeAction"
-)
-
 public class DeleteUserAction extends HttpServlet {
+
+    private final Logger log = Logger.getLogger(this.getClass());
 
     /**
      * Handles HTTP post request
@@ -33,6 +30,11 @@ public class DeleteUserAction extends HttpServlet {
      * @throws IOException if there is an IO failure
      */
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //TODO access userId
+        //TODO access userRoleId
+        //TODO call deleteUser from userdao
+        //TODO call deleteUserRole from userroledao
 
         redirectOnSubmit(response);
     }
@@ -46,6 +48,7 @@ public class DeleteUserAction extends HttpServlet {
      */
     public void redirectOnSubmit(HttpServletResponse response) throws ServletException, IOException {
 
+        log.info("accessed user delete");
         String url = "/chef/manageEmployees";
         response.sendRedirect(url);
     }
