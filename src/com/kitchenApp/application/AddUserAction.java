4b7565271c356f2318a3639821aa08fdaadf3e9d
afaherty27 <afaherty27@gmail.com>
2015-11-30 01:51:08 -0600

    initial commit

---
+++ src/com/kitchenApp/application/AddUserAction.java
@@ -0,0 +1,12 @@
+package com.kitchenApp.application;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author afaherty
+ * @version 1.0 on 11/30/2015
+ */
+public class AddUserAction {
+
+    private final Logger log = Logger.getLogger(this.getClass());
+}
