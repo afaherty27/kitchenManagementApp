0fad15185efba03f20144d3060356dff1eccc68a
afaherty27 <afaherty27@gmail.com>
2015-11-30 03:06:41 -0600

    initial commit

--- src/com/kitchenApp/application/UpdateUserAction.java
+++ src/com/kitchenApp/application/UpdateUserAction.java
@@ -1,54 +1,8 @@
 package com.kitchenApp.application;
 
-import org.apache.log4j.Logger;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@WebServlet(
-        name = "updateUser",
-        urlPatterns = {"/chef/updateUser"}
-)
 /**
- * Handles user data updates
  * @author afaherty
- * @version 1.0 on 11/27/2015
  */
-public class UpdateUserAction extends HttpServlet {
 
-    private final Logger log = Logger.getLogger(this.getClass());
-
-    /**
-     * Handles HTTP post request
-     * @param request the HttpServletRequest object
-     * @param response the HttpServletResponse object
-     * @throws ServletException if there is a Servlet failure
-     * @throws IOException if there is an IO failure
-     */
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        //TODO access userId
-        //TODO access userRoleId
-        //TODO call deleteUser from userdao
-        //TODO call deleteUserRole from userroledao
-
-        redirectOnSubmit(response);
-    }
-
-    /**
-     /**
-     * Redirects user to clean form after submit. posts message for successful add
-     * @param response HttpServletResponse object
-     * @throws ServletException if there is a servlet error
-     * @throws IOException if there is an input/output error
-     */
-    public void redirectOnSubmit(HttpServletResponse response) throws ServletException, IOException {
-
-        log.info("accessed user delete");
-        String url = "/chef/manageEmployees";
-        response.sendRedirect(url);
-    }
+public class UpdateUserAction {
+}
