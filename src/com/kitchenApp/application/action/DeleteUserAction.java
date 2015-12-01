7f7ef09a5c18a41166e5fce912ce31f8123db412
afaherty27 <afaherty27@gmail.com>
2015-12-01 04:06:17 -0600

    completed delete action

--- src/com/kitchenApp/application/action/DeleteUserAction.java
+++ src/com/kitchenApp/application/action/DeleteUserAction.java
@@ -1,5 +1,7 @@
 package com.kitchenApp.application.action;
 
+import com.kitchenApp.database.dataAccess.UserDao;
+import com.kitchenApp.database.dataAccess.UserRoleDao;
 import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
@@ -9,10 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(
-        name = "deleteUser",
-        urlPatterns = "/chef/deleteUser"
-)
+
 
 /**
  * @author afaherty
@@ -22,34 +21,44 @@ public class DeleteUserAction extends HttpServlet {
 
     private final Logger log = Logger.getLogger(this.getClass());
 
-    /**
-     * Handles HTTP post request
-     * @param request the HttpServletRequest object
-     * @param response the HttpServletResponse object
-     * @throws ServletException if there is a Servlet failure
-     * @throws IOException if there is an IO failure
-     */
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        //TODO access userId
-        //TODO access userRoleId
-        //TODO call deleteUser from userdao
-        //TODO call deleteUserRole from userroledao
-
-        redirectOnSubmit(response);
-    }
+    private UserDao userDao;
+    private UserRoleDao userRoleDao;
 
     /**
-    /**
-     * Redirects user to clean form after submit. posts message for successful add
-     * @param response HttpServletResponse object
+     * updates user data to kitchenapp database user table
+     * @param request HttpServletRequest object
      * @throws ServletException if there is a servlet error
      * @throws IOException if there is an input/output error
      */
-    public void redirectOnSubmit(HttpServletResponse response) throws ServletException, IOException {
+    public UserDao deleteUserData(HttpServletRequest request) throws ServletException, IOException {
 
-        log.info("accessed user delete");
-        String url = "/chef/manageEmployees";
-        response.sendRedirect(url);
+        String userId = request.getParameter("selectDelete");
+        int idInt = Integer.parseInt(userId);
+
+        userDao = new UserDao();
+        userDao.deleteUser(idInt);
+
+        log.info("deleting user");
+
+        return userDao;
+    }
+
+    /**
+     * update user role data to kitchenapp database user_role table
+     * @param request HttpServletRequest object
+     * @throws ServletException if there is a servlet error
+     * @throws IOException if there is an input/output error
+     */
+    public UserRoleDao deleteUserRoleData(HttpServletRequest request) throws ServletException, IOException {
+
+        String userId = request.getParameter("selectDelete");
+        int idInt = Integer.parseInt(userId);
+
+        userRoleDao = new UserRoleDao();
+        userRoleDao.deleteUserRole(idInt);
+
+        log.info("deleting userrole");
+
+        return userRoleDao;
     }
 }
