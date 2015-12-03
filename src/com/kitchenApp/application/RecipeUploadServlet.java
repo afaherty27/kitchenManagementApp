c71d711b988743ae497e415ca2a15b44b0ba1779
afaherty27 <afaherty27@gmail.com>
2015-12-02 23:50:17 -0600

    initial commit

---
+++ src/com/kitchenApp/application/RecipeUploadServlet.java
@@ -0,0 +1,21 @@
+package com.kitchenApp.application;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author afaherty
+ * @version 1.0 on 12/2/2015
+ */
+
+public class RecipeUploadServlet extends HttpServlet {
+
+    // todo inst var for RecipeFileUploader
+
+    // todo dopost
+
+    // todo redirect to upload page
+
+
+
+
+}
