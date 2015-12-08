014c5b80c8d7a3c775977e5f29132b7706eaf19c
afaherty27 <afaherty27@gmail.com>
2015-12-07 20:56:07 -0600

    file upload

--- src/com/kitchenApp/application/RecipeUploadServlet.java
+++ src/com/kitchenApp/application/RecipeUploadServlet.java
@@ -1,21 +1,77 @@
 package com.kitchenApp.application;
 
-import javax.servlet.http.HttpServlet;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
-/**
- * @author afaherty
- * @version 1.0 on 12/2/2015
- */
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(
+        name = "recipeUploadServlet",
+        urlPatterns = { "/chef/uploadRecipe" }
+)
 
 public class RecipeUploadServlet extends HttpServlet {
 
-    // todo inst var for RecipeFileUploader
+    private final String UPLOAD_DIRECTORY = "C:/tomcat/webapps/upload"; //set up in properties file.
 
-    // todo dopost
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    // todo redirect to upload page
+        //process only if its multipart content
+        if(ServletFileUpload.isMultipartContent(request)){
 
+            try {
+                List<FileItem> fileList = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
 
+                for(FileItem item : fileList){
 
+                    if(!item.isFormField()){
 
+                        String fileName = new File(item.getName()).getName();
+
+                        item.write( new File(UPLOAD_DIRECTORY + File.separator + fileName));
+                        placePathInSession(request, fileName);
+                    }
+                }
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+
+        }else{
+
+            request.setAttribute("message",
+
+                    "Sorry this Servlet only handles file upload request");
+
+        }
+
+        //add file path to session
+
+        forwardToResultsPage(request, response);
+    }
+
+    public void forwardToResultsPage(HttpServletResponse response) throws ServletException, IOException {
+
+        String url = "/chef";
+        response.sendRedirect(url);
+    }
+
+    public void placePathInSession(HttpServletRequest request, String fileName) {
+
+        String fileLocation = UPLOAD_DIRECTORY + fileName;
+
+        HttpSession session = request.getSession();
+        session.setAttribute("uploadedFilePath", fileLocation);
+    }
 }
