7696bd1e6d1f51c91b84681e9673d3860b2ad321
afaherty27 <afaherty27@gmail.com>
2015-12-08 02:20:19 -0600

    package refactor

--- src/com/kitchenApp/application/recipe/RecipeUploadServlet.java
+++ src/com/kitchenApp/application/recipe/RecipeUploadServlet.java
@@ -1,4 +1,4 @@
-package com.kitchenApp.application;
+package com.kitchenApp.application.recipe;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
