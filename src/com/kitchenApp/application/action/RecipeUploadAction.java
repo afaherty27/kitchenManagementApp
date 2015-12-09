d473de516503f1fb38cb32d7006895acf62236e0
afaherty27 <afaherty27@gmail.com>
2015-12-09 01:22:08 -0600

    completed upload action

--- src/com/kitchenApp/application/action/RecipeUploadAction.java
+++ src/com/kitchenApp/application/action/RecipeUploadAction.java
@@ -1,10 +1,11 @@
 package com.kitchenApp.application.action;
 
+import com.kitchenApp.database.dataAccess.RecipeDao;
 import com.kitchenApp.database.entity.Recipe;
 import org.apache.log4j.Logger;
 
 /**
- * Adds recipe to database
+ * Adds recipe data to database
  * @author afaherty
  * @version 1.0 on 12/8/2015
  */
@@ -14,10 +15,22 @@ public class RecipeUploadAction {
 
     private final Logger log = Logger.getLogger(this.getClass());
 
-    public Recipe addRecipe() {
+    /**
+     * adds recipe data to database
+     * @param filePath ref to recipe file location
+     * @param category ref to category of recipe
+     * @param recipeName ref to recipe name
+     * @return recipe
+     */
+    public Recipe addRecipeData(String filePath, String category, String recipeName) {
 
+        RecipeDao dao = new RecipeDao();
+        Recipe recipe = new Recipe(ID_PLACEHOLDER, filePath, category, recipeName);
 
+        dao.addRecipe(recipe);
 
+        log.info(recipe.toString());
 
+        return recipe;
     }
 }
