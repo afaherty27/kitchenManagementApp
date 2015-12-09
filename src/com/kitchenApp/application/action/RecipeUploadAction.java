7c2f8a210a0ad8e41903b190f949b2a154bd861b
afaherty27 <afaherty27@gmail.com>
2015-12-08 22:23:48 -0600

    initial commit

---
+++ src/com/kitchenApp/application/action/RecipeUploadAction.java
@@ -0,0 +1,23 @@
+package com.kitchenApp.application.action;
+
+import com.kitchenApp.database.entity.Recipe;
+import org.apache.log4j.Logger;
+
+/**
+ * Adds recipe to database
+ * @author afaherty
+ * @version 1.0 on 12/8/2015
+ */
+public class RecipeUploadAction {
+
+    private static final int ID_PLACEHOLDER = 0; //for hibernate auto increment
+
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    public Recipe addRecipe() {
+
+
+
+
+    }
+}
