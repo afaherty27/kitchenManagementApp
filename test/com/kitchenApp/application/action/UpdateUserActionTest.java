30482720d1ff6a7cf58308d52a3216d2ba609425
afaherty27 <afaherty27@gmail.com>
2015-12-17 23:22:02 -0600

    completed UpdateUserAction tests

--- test/com/kitchenApp/application/action/UpdateUserActionTest.java
+++ test/com/kitchenApp/application/action/UpdateUserActionTest.java
@@ -1,5 +1,11 @@
 package com.kitchenApp.application.action;
 
+import com.kitchenApp.database.dataAccess.UserDao;
+import com.kitchenApp.database.dataAccess.UserRoleDao;
+import com.kitchenApp.database.entity.User;
+import com.kitchenApp.database.entity.UserRole;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -10,4 +16,70 @@ import static org.junit.Assert.*;
  */
 public class UpdateUserActionTest {
 
+    private User user;
+    private UserDao userDao = new UserDao();
+    private UserRole role;
+    private UserRoleDao userRoleDao = new UserRoleDao();
+    private UpdateUserAction update = new UpdateUserAction();
+
+    @Before
+    public void before() {
+        role = new UserRole(0, "refRole", "refUserName");
+        user = new User(0, "refUserName", "refPass", "refAddress", "refPhone", "refEmail", role);
+        userRoleDao.addUserRole(role);
+        userDao.addUser(user);
+    }
+
+    @Test
+    public void updateUserData() {
+
+        String idString = String.valueOf(user.getUserId());
+        String preUpdate = String.valueOf(userDao.getUser(user.getUserId()));
+
+        update.updateUserData(idString, "updateAddress", "updatePhone", "updateEmail");
+
+        String postUpdate = String.valueOf(userDao.getUser(user.getUserId()));
+
+        assertTrue("updating incorrect user id", idString.equals(String.valueOf(user.getUserId())));
+        assertFalse("user data update failed", preUpdate.equals(postUpdate));
+        assertTrue("UpdateUserAction object not instantiated", update != null);
+
+    }
+
+    @Test
+    public void updateUserRoleData() {
+
+        String idString = String.valueOf(role.getRoleId());
+        String preUpdate = String.valueOf(userRoleDao.getUserRole(role.getRoleId()));
+
+        update.updateUserRoleData(idString, "updateRole");
+
+        String postUpdate = String.valueOf(userRoleDao.getUserRole(role.getRoleId()));
+
+        assertTrue("updating incorrect user role id", idString.equals(String.valueOf(role.getRoleId())));
+        assertFalse("user role data update failed", preUpdate.equals(postUpdate));
+        assertNotNull("UpdateUserAction object not instantiated", update);
+    }
+
+    /**
+     * Cleans test data out of database
+     */
+    @After
+    public void after() {
+
+        userDao.deleteUser(user.getUserId());
+        userRoleDao.deleteUserRole((role.getRoleId()));
+
+//        UserDao userDao = new UserDao();
+//        UserRoleDao userRoleDao = new UserRoleDao();
+//        UserRole role = new UserRole(0, "ref", "ref"); //to create a ref to delete all test users at end
+//        User user = new User(0, "ref", "ref", "ref", "ref", "ref", role); //to create a ref to delete all test users at end
+//        userRoleDao.addUserRole(role);
+//        userDao.addUser(user);
+//
+//        userDao.deleteUser(user.getUserId());
+//        userDao.deleteUser(user.getUserId() - 1);
+//        userRoleDao.deleteUserRole(role.getRoleId());
+//        userRoleDao.deleteUserRole(role.getRoleId() - 1);
+    }
 }
