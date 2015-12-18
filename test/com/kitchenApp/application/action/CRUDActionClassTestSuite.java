389823cd689c7251d00d955c316ba00d77db6c65
afaherty27 <afaherty27@gmail.com>
2015-12-17 23:15:36 -0600

    initial commit

---
+++ test/com/kitchenApp/application/action/CRUDActionClassTestSuite.java
@@ -0,0 +1,12 @@
+package com.kitchenApp.application.action;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Test Suite for CRUD action classes
+ * @author afaherty
+ * @version 1.0 on 12/17/2015
+ */
+
+public class CRUDActionClassTestSuite {}
