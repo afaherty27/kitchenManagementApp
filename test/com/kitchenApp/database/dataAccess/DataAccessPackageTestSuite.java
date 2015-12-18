9937ca8619adfd6a1e1f47e67fb601562e538d5d
afaherty27 <afaherty27@gmail.com>
2015-12-18 01:23:16 -0600

    initial commit

---
+++ test/com/kitchenApp/database/dataAccess/DataAccessPackageTestSuite.java
@@ -0,0 +1,14 @@
+package com.kitchenApp.database.dataAccess;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Test suite for classes in com.kitchenApp.database.dataAccess package
+ * @author afaherty
+ * @version 1.0 on 12/18/2015
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ })
+public class DataAccessPackageTestSuite {
+}
---
+++ test/com/kitchenApp/database/dataAccess/UserDaoTest.java
@@ -0,0 +1,13 @@
+package com.kitchenApp.database.dataAccess;
+
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test class for UserDao class
+ * @author afaherty
+ * @version 1.0 on 12/18/2015
+ */
+public class UserDaoTest {
+}
