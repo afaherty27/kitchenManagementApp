b0e5b707f43edecac470154d80fa2b81fee93730
afaherty27 <afaherty27@gmail.com>
2015-12-18 01:08:15 -0600

    addRecipeData method test completed

--- test/com/kitchenApp/application/action/RecipeUploadActionTest.java
+++ test/com/kitchenApp/application/action/RecipeUploadActionTest.java
@@ -1,5 +1,17 @@
 package com.kitchenApp.application.action;
 
+import com.kitchenApp.database.dataAccess.RecipeDao;
+import com.kitchenApp.database.entity.Recipe;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.servlet.http.Part;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
 /**
  * Test class for RecipeUploadAction
  * @author afaherty
@@ -7,5 +19,36 @@ package com.kitchenApp.application.action;
  */
 public class RecipeUploadActionTest {
 
+    private String filePath = "C:/testRecipePath";
+    private String category = "testCategory";
+    private String recipeName = "test name";
+    private Recipe recipe;
+    private RecipeDao dao;
 
+   @Before
+   public void before() {
+       recipe = new Recipe(0, filePath, category, recipeName);
+       dao = new RecipeDao();
+       dao.addRecipe(recipe);
+   }
+
+    @Test
+    public void addRecipeData() {
+
+        RecipeUploadAction upload = new RecipeUploadAction();
+
+        int listLength = dao.getRecipeList().size();
+
+        upload.addRecipeData(filePath, category, recipeName);
+
+        assertNotNull(upload);
+        assertTrue("list size should be larger than held size", listLength < dao.getRecipeList().size() );
+    }
+
+    @After
+    public void after() {
+
+        dao.deleteRecipe(recipe.getRecipeId());
+        dao.deleteRecipe(recipe.getRecipeId() - 1);
+    }
 }
