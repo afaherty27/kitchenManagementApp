ab5298067989bc4efd7af2571ea109313516af7c
afaherty27 <afaherty27@gmail.com>
2015-12-18 00:13:54 -0600

    Replacing corrupted file

---
+++ test/com/kitchenApp/application/action/ActionPackageTestSuite.java
@@ -0,0 +1,17 @@
+package com.kitchenApp.application.action;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Test Suite for CRUD action classes
+ * @author afaherty
+ * @version 1.0 on 12/17/2015
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AddUserActionTest.class,
+        UpdateUserActionTest.class,
+        DeleteUserActionTest.class
+})
+public class ActionPackageTestSuite { }
