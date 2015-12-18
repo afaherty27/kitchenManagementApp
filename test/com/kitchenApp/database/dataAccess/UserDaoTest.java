9c8db53a9e7b6a736277ce136b115205134650f8
afaherty27 <afaherty27@gmail.com>
2015-12-18 02:49:37 -0600

    addUser tests completed

--- test/com/kitchenApp/database/dataAccess/UserDaoTest.java
+++ test/com/kitchenApp/database/dataAccess/UserDaoTest.java
@@ -1,6 +1,11 @@
 package com.kitchenApp.database.dataAccess;
 
+import com.kitchenApp.database.entity.User;
+import com.kitchenApp.database.entity.UserRole;
+import org.hibernate.HibernateException;
 import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -11,12 +16,42 @@ import static org.junit.Assert.*;
  */
 public class UserDaoTest {
 
+    private UserRole role;
+    private UserRoleDao roleDao;
+
+    /**
+     * Instantiates a UserRole object to be used in each method. Adds to database.
+     */
+    @Before
+    public void before() {
+        role = new UserRole(0, "role", "name");
+        roleDao = new UserRoleDao();
+        roleDao.addUserRole(role);
+    }
+
     /**
      * method test for addUser method
      */
     @Test
-    public void addUser() {
+    public void addUser() throws HibernateException {
 
+        UserDao dao = new UserDao();
+        User user = new User(0, "test", "test", "test", "test", "test", role);
+
+        try {
+            dao.addUser(user);
+
+            assertEquals("New User:  test test test test test " + role.getRoleId() ,
+                    String.valueOf(dao.getUser(user.getUserId())));
+            assertNotNull("integer is null", user.getUserId());
+
+
+        } catch (HibernateException e) {
+            fail("hibernate exception was caught");
+        }
+
+        dao.deleteUser(user.getUserId());
+        roleDao.deleteUserRole(role.getRoleId());
     }
 
     /**
