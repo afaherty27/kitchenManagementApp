014eb79cad4dbbbee85769696379a5ee889ebcaf
afaherty27 <afaherty27@gmail.com>
2015-12-17 21:48:02 -0600

    initial commit

---
+++ test/com/kitchenApp/application/action/DeleteUserActionTest.java
@@ -0,0 +1,13 @@
+package com.kitchenApp.application.action;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test Class for DeleteUserAction class
+ * @author afaherty
+ * @version 1.0 on 12/17/2015
+ */
+public class DeleteUserActionTest {
+
+}
---
+++ test/com/kitchenApp/application/action/UpdateUserActionTest.java
@@ -0,0 +1,13 @@
+package com.kitchenApp.application.action;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test class for UpdateUserAction class
+ * @author afaherty
+ * @version 1.0 on 12/17/2015
+ */
+public class UpdateUserActionTest {
+
+}
