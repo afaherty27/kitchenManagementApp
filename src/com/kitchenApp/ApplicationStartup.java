57af7dd0ca95bbe5cffbe02bf4cd958d557a1885
afaherty27 <afaherty27@gmail.com>
2015-12-20 00:04:55 -0600

    initial commit

---
+++ src/com/kitchenApp/ApplicationStartup.java
@@ -0,0 +1,35 @@
+package com.kitchenApp;
+
+import com.kitchenApp.database.dataAccess.UserDao;
+import com.kitchenApp.database.dataAccess.UserRoleDao;
+import org.apache.log4j.Logger;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.util.Properties;
+
+@WebServlet(
+        name = "appStartup",
+        urlPatterns = "/kitchenAppStartup",
+        loadOnStartup = 1
+)
+/**
+ * Initializes data for  application
+ * @author afaherty
+ * @version 10 on 12/19/2015
+ */
+public class ApplicationStartup extends HttpServlet {
+
+    private Properties properties;
+    private UserDao userDao;
+    private UserRoleDao userRoleDao;
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    public void init() throws ServletException {
+
+    }
+}
